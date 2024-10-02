13519b04b7b105e4eb2546f9f096ee588a67d3be
arusrex <arusbr@hotmail.com>
2024-10-02 19:23:14 -0300

    att

--- sitesetup/context_processors.py
+++ sitesetup/context_processors.py
@@ -13,21 +13,27 @@ def site_settings(request):
     return context
 
 def menu_settings(request):
-    # try:
-    #     menu = Menu.objects.all()
-    # except Menu.DoesNotExist:
-    #     menu = None
-    #     print('Erro: Não existem menus, ou não foram cadastrados')
+    try:
+        menu = Menu.objects.all()
+    except Menu.DoesNotExist:
+        menu = None
+        print('Erro: Não existem menus, ou não foram cadastrados')
 
-    # try:
-    #     sub_menu = SubMenu.objects.all()
-    # except SubMenu.DoesNotExist:
-    #     sub_menu = None
-    #     print('Erro: Não existem sub-menus, ou não foram cadastrados')
-    # try:
-    #     sub_sub_menu = SubSubMenu.objects.all()
-    # except SubSubMenu.DoesNotExist:
-    #     sub_sub_menu = None
-    #     print('Erro: Não existem sub-sub-menus, ou não foram cadastrados')
-    ...
-    
+    try:
+        sub_menu = SubMenu.objects.all()
+    except SubMenu.DoesNotExist:
+        sub_menu = None
+        print('Erro: Não existem sub-menus, ou não foram cadastrados')
+    try:
+        sub_sub_menu = SubSubMenu.objects.all()
+    except SubSubMenu.DoesNotExist:
+        sub_sub_menu = None
+        print('Erro: Não existem sub-sub-menus, ou não foram cadastrados')
+
+    context = {
+        'menu': menu,
+        'sub_menu': sub_menu,
+        'sub_sub_menu': sub_sub_menu,
+    }
+
+    return context
