01b06632b344fb0e352e55b1a2e1780261d6d72d
arusrex <arusbr@hotmail.com>
2024-10-15 11:40:50 -0300

    melhorias no dark-mode

--- control_panel/static/js/area-chart.js
+++ control_panel/static/js/area-chart.js
@@ -1,24 +1,28 @@
 window.addEventListener('DOMContentLoaded', function() {
     const ctx = document.getElementById('areaChart');
 
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Gray'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3, 9],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                y: {
-                    beginAtZero: true
+    if (ctx) {
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Gray'],
+                datasets: [{
+                    label: '# of Votes',
+                    // backgroundColor:"rgba(0,0,255,1.0)",
+                    // borderColor: "rgba(0,0,255,0.1)",
+                    data: [12, 19, 3, 5, 2, 3, 9],
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
+        });
+    };
 });
 
--- control_panel/static/js/scripts.js
+++ control_panel/static/js/scripts.js
@@ -26,6 +26,56 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
+// DARK MODE
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+    const tableDark = document.querySelector('#dataTable');
+    const cardDivs = document.querySelectorAll('.card-div-dark');
+    const formsDarks = document.querySelectorAll('.form-control');
+    const mainDark = document.querySelector('main');
+    const footerDark = document.querySelector('footer');
 
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        document.body.classList.add('bg-dark', 'text-secondary');
+        mainDark.classList.add('border','border-light', 'rounded');
+        footerDark.classList.add('bg-dark', 'text-light');
+        if (tableDark){tableDark.classList.add('table-dark');};
+        cardDivs.forEach(cardDiv => cardDiv.classList.add('card-dark',));
+        formsDarks.forEach(formsDark => formsDark.classList.add('card-dark'));
+    };
 
+    if (toggleButton) {
+        toggleButton.addEventListener('click', () => {
+            document.body.classList.toggle('bg-dark');
+            document.body.classList.toggle('text-secondary');
+
+            mainDark.classList.toggle('border');
+            mainDark.classList.toggle('border-light');
+            mainDark.classList.toggle('rounded');
+
+            footerDark.classList.toggle('bg-dark');
+            footerDark.classList.toggle('text-light');
+
+            if (tableDark) {
+                tableDark.classList.toggle('table-dark');
+            };
+    
+            cardDivs.forEach(cardDiv => {
+                cardDiv.classList.remove('card-dark');
+            });
+    
+            formsDarks.forEach(formsDark => {
+                formsDark.classList.remove('card-dark');
+            });
+    
+            if (document.body.classList.contains('bg-dark')) {
+                localStorage.setItem('dark-mode', 'enabled');
+            } else {
+                localStorage.setItem('dark-mode', 'disabled');
+            };
+
+            location.reload()
+        });
+    };    
+});
 
